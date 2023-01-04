07903135df7106bec61567f551e6e721cd96d121
Aubert Nicolas <nicolas.aubert@he-arc.ch>
2023-01-05 00:39:34 +0100

    Adding base controller + Updating configuration

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # walkingfish_spring
+
+### How to build and run the app
+
+```
+./mvnw clean install
+./mvnw package # Useful ?
+
+./mvnw spring-boot:run
+
+# or
+
+java -jar target/<app-name>.war
---
+++ src/main/java/ch/walkingfish/walkingfish/controller/IndexController.java
@@ -0,0 +1,16 @@
+package main.java.ch.walkingfish.walkingfish;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class IndexController {
+
+    @GetMapping(value = {"/","/accueil"})
+    public String index(Model model) {
+        return "index";
+    }
+}
