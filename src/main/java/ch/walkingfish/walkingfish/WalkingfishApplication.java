1a4cbd3cacbcdb1cdac4c4612e2bb3341b18db11
Aubert Nicolas <nicolas.aubert@he-arc.ch>
2023-01-04 16:44:55 +0100

    Adding base project

---
+++ src/main/java/ch/walkingfish/walkingfish/WalkingfishApplication.java
@@ -0,0 +1,13 @@
+package ch.walkingfish.walkingfish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkingfishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkingfishApplication.class, args);
+	}
+
+}
