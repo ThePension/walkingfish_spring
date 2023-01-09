799bb52e20e281fefbf8e5c37786aabbf82e69c3
Nicolas Aubert <nicolas.aubert1@he-arc.ch>
2023-01-09 11:14:37 +0100

    Adding first unit test

--- src/main/java/ch/walkingfish/walkingfish/controller/IndexController.java
+++ src/main/java/ch/walkingfish/walkingfish/controller/IndexController.java
@@ -1,4 +1,4 @@
-package main.java.ch.walkingfish.walkingfish;
+package ch.walkingfish.walkingfish.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/test/java/ch/walkingfish/walkingfish/WalkingfishApplicationTests.java
+++ src/test/java/ch/walkingfish/walkingfish/WalkingfishApplicationTests.java
@@ -1,13 +1,31 @@
 package ch.walkingfish.walkingfish;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import ch.walkingfish.walkingfish.controller.IndexController;
+
+@WebMvcTest(controllers = IndexController.class)
 class WalkingfishApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
+
+	// @Autowired
+	// private IndexController indexController;
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		this.mockMvc
+				.perform(get("/")) //
+				.andDo(print()) //
+				.andExpect(status().isOk()); // .andExpect(content().string("index"));
 	}
 
 }
