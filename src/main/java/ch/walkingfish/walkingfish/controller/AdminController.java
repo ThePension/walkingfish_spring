ef14ca01ef00616ec241453a25b72aa6f26b9a52
Nicolas Aubert <nicolas.aubert1@he-arc.ch>
2023-01-27 14:20:47 +0100

    Updating report

--- src/main/java/ch/walkingfish/walkingfish/controller/AdminController.java
+++ src/main/java/ch/walkingfish/walkingfish/controller/AdminController.java
@@ -37,7 +37,7 @@ public class AdminController {
     @Autowired
     FileStorageService fileStorageService;
 
-    private final int pageSize = 6;
+    private final int pageSize = 3;
 
     /**
      * Show the catalogue
